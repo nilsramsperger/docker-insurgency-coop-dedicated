9d022cabcb1e54b55d8cf444dc4c5d120297ec88
nils <nils@ramsperger-home.de>
2019-01-31 18:08:41 +0100

    Changed install condition.

--- files/supervisor.sh
+++ files/supervisor.sh
@@ -14,15 +14,16 @@ term_handler() {
 install() {
     echo "Installing Insurgency Dedicated Server"
     /opt/steam/steamcmd.sh +login anonymous +force_install_dir /opt/steam/ +app_update 237410 validate +quit
-    mv /tmp/cfg /opt/steam/insurgency
-    mv /tmp/scripts /opt/steam/insurgency
+    mkdir /opt/steam/insurgency/scripts
+    cp -a /tmp/cfg/. /opt/steam/insurgency/cfg/
+    cp -a /tmp/scripts/. /opt/steam/insurgency/scripts/
     rm /tmp/
     chown -R steam:steam /opt/steam/insurgency
     echo "Installation done"
 }
 
 trap term_handler SIGTERM
-[ ! -d "/opt/steam/insurgency" ] && install
+[ ! -d "/opt/steam/insurgency/scripts" ] && install
 echo "Starting Insurgency Dedicated Server"
 cd /opt/steam/insurgency
 su steam
